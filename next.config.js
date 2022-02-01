3cc04e1bc86e03e26e8559951c79d772e0d1d9b3
Angel <82230757+angelhtml@users.noreply.github.com>
2022-02-01 14:09:15 -0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,7 @@
-/*
+
 module.exports = {
   //reactStrictMode: true,
   basePath: '/dogenews',
   assetPrefix: '/dogenews/'
 }
-*/
-const ghPages = process.env.DEPLOY_TARGET === 'gh-pages';
 
-module.exports = {
-  assetPrefix: ghPages ? '/dotcs.github.io/' : ''   // customize this value
-};
