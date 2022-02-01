43145b0828b4ab365f39119bf51dcbaa577612cc
Angel <82230757+angelhtml@users.noreply.github.com>
2022-02-01 04:30:39 -0800

    Update Footer.jsx

--- dogecompaile/Footer.jsx
+++ dogecompaile/Footer.jsx
@@ -2,38 +2,38 @@ import { Center, HStack, VStack } from "@chakra-ui/layout";
 import {FaTwitter } from 'react-icons/fa';
 import {FaInstagram } from 'react-icons/fa';
 import {SiDiscord } from 'react-icons/si';
-import footerStyle from "../styles/footer.module.css"
+import FooterStyle from "../styles/Footer.module.css"
 
 function Footer(){
     return(
         <div>
-            <footer class={footerStyle.footer}>
-                    <div class={footerStyle.container}>
-                        <div class={footerStyle.socailmedia}>
+            <footer class={FooterStyle.footer}>
+                    <div class={FooterStyle.container}>
+                        <div class={FooterStyle.socailmedia}>
                             <center>
-                            <h1 class={footerStyle.socailtitle}>Community</h1>
+                            <h1 class={FooterStyle.socailtitle}>Community</h1>
                             </center>
                             <VStack marginTop='1rem'>
-                            <a href='#' class={footerStyle.href}>Terms & Conditions </a>
-                            <a href='#' class={footerStyle.href}>Market</a>
+                            <a href='#' class={FooterStyle.href}>Terms & Conditions </a>
+                            <a href='#' class={FooterStyle.href}>Market</a>
                             </VStack>
                         </div>
-                        <div class={footerStyle.socailmedia}>
+                        <div class={FooterStyle.socailmedia}>
                             <center>
-                            <h1 class={footerStyle.about}>About us</h1>
+                            <h1 class={FooterStyle.about}>About us</h1>
                             </center>
                             <VStack marginTop='1rem'>
-                            <p class={footerStyle.href} style={{cursor:"pointer"}}>Roadmap</p>
-                            <a href='#' class={footerStyle.href}>FAQ</a>
+                            <p class={FooterStyle.href} style={{cursor:"pointer"}}>Roadmap</p>
+                            <a href='#' class={FooterStyle.href}>FAQ</a>
                             </VStack>
                         </div>
                     </div>
                     <center>
                         <Center>
                         <HStack padding='1.5rem' spacing='1rem'>
-                        <a href='#'><SiDiscord class={footerStyle.link}/></a>
-                        <a href='#'><FaTwitter class={footerStyle.link}/></a>
-                        <a href='#'><FaInstagram class={footerStyle.link}/></a>
+                        <a href='#'><SiDiscord class={FooterStyle.link}/></a>
+                        <a href='#'><FaTwitter class={FooterStyle.link}/></a>
+                        <a href='#'><FaInstagram class={FooterStyle.link}/></a>
                         </HStack>
                         </Center>
                     <hr color='#a200ff' width='85%' marginTop='2.5rem'/>
