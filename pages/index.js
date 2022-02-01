ad2653424fd0981819ecbbef0f1516857830beb7
Angel <82230757+angelhtml@users.noreply.github.com>
2022-02-01 04:24:38 -0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import { Image } from '@chakra-ui/image';
 import { BiSearch } from 'react-icons/bi';
 import { SiDogecoin } from 'react-icons/si';
 import { ChakraProvider,Input,InputGroup,InputLeftElement,HStack } from '@chakra-ui/react';
-import homeStyle from '../styles/home.module.css';
+import HomeStyle from '../styles/home.module.css';
 import Tabbar from '../dogecompaile/Tabbar';
 import Footer from '../dogecompaile/Footer';
 import Marquee from "react-fast-marquee";
@@ -94,8 +94,8 @@ function extra (index){
   //=========================================================================================================
   return (
     <div>
-            <div className={homeStyle.navbar}>
-        <div className={homeStyle.navbar1}>
+            <div className={HomeStyle.navbar}>
+        <div className={HomeStyle.navbar1}>
           <HStack marginTop="0.3rem" marginLeft="1rem">
           <SiDogecoin style={{color:"yellow",fontSize:"1.5rem"}}/>
           <p color="white">Doge news</p>
@@ -106,8 +106,8 @@ function extra (index){
                 </InputGroup>
             </HStack>
         </div>
-        <div className={homeStyle.navbar2}>
-        <div className={homeStyle.links}>
+        <div className={HomeStyle.navbar2}>
+        <div className={HomeStyle.links}>
             <a href="#">News</a>
             <a href="#">Market</a>
             <a onClick={() => router.push('/pumps')}>opinion</a>
@@ -115,20 +115,20 @@ function extra (index){
         </div>
       </div>
 
-      <div className={homeStyle.mobilenav}><center><SiDogecoin style={{color:"yellow",fontSize:"1.5rem",position:'relative',top:'0.2rem'}}/></center></div>
+      <div className={HomeStyle.mobilenav}><center><SiDogecoin style={{color:"yellow",fontSize:"1.5rem",position:'relative',top:'0.2rem'}}/></center></div>
 
-      <div><Image src="1635779886.gif" className={homeStyle.ad}/></div>
+      <div><Image src="1635779886.gif" className={HomeStyle.ad}/></div>
       <Marquee gradientColor='0, 2, 92' speed='50' style={{textShadow:"0 1px 1px yellow",marginTop:"1rem"}}><pre style={{display:"flex"}}>                 DOGE  {dogeico}{dogeprice}  {dogeico}%{dogepercentchange24h} 24h                 BTC  {bitcoinico}{bitcoinprice}  {bitcoinico}%{bitcoinpercentchange24h} 24h</pre></Marquee>
 
 
       { newsListLoading  ? <>
       
       <center>
-      <div className={homeStyle.newsbox} marginTop="2rem">
+      <div className={HomeStyle.newsbox} marginTop="2rem">
       {Loading && news.map((news, index)=>
       <div key={news}>
         
-      <div className={homeStyle.newsbox1} onClick={() => setmore(()=> extra(index))}><center><div className={homeStyle.news}><Image src='retro-bg.jpg' width="100%"/></div></center><p className={homeStyle.title}>{news.title.substring(0, 50)}</p></div>
+      <div className={HomeStyle.newsbox1} onClick={() => setmore(()=> extra(index))}><center><div className={HomeStyle.news}><Image src='retro-bg.jpg' width="100%"/></div></center><p className={HomeStyle.title}>{news.title.substring(0, 50)}</p></div>
       
       </div>)}
       </div>
@@ -144,12 +144,12 @@ function extra (index){
 
 
       {more && 
-      <div style={{position:"fixed" , height:"100%" , width:"100%" , background:"rgba(195, 121, 255, 0.645)", top:"0" , left:"0"}} onClick={() => setmore(!more)} className={homeStyle.boxanime}>
+      <div style={{position:"fixed" , height:"100%" , width:"100%" , background:"rgba(195, 121, 255, 0.645)", top:"0" , left:"0"}} onClick={() => setmore(!more)} className={HomeStyle.boxanime}>
        <div style={{height :"100vh", position:"fixed" ,overflow:"auto",marginLeft:"5%" ,top:"5vh" ,width: "90vw" , background:"#0c0029" ,zIndex:"99999",borderRadius:'15px'}} overflow='auto' onClick={(e) => e.stopPropagation()}>
-       <p className={homeStyle.title}>{news[numindex].title}</p>
-       <div className={homeStyle.infobox}>
-       <div className={homeStyle.infobox1}><Image src='retro-bg.jpg'/><div style={{marginTop:'1rem'}}><span style={{padding:"1rem",textShadow:"0 1px 1px #f0f"}}>published at:</span><span style={{textShadow:"0 1px 1px black"}}>{news[numindex].published}</span></div><div><span style={{padding:"1rem",textShadow:"0 1px 1px #f0f"}}>Author:</span><span style={{textShadow:"0 1px 1px black"}}></span></div></div>
-       <div className={homeStyle.infobox2}><p style={{padding:"1rem",textShadow:"0 1px 1px #f0f"}}>description:</p><p  style={{padding:"1rem",textShadow:"0 1px 1px black"}}>{news[numindex].title.substring(0, 160)}...</p><center><a href={news[numindex].link} className={homeStyle.seemorelink}>See more</a></center></div>
+       <p className={HomeStyle.title}>{news[numindex].title}</p>
+       <div className={HomeStyle.infobox}>
+       <div className={HomeStyle.infobox1}><Image src='retro-bg.jpg'/><div style={{marginTop:'1rem'}}><span style={{padding:"1rem",textShadow:"0 1px 1px #f0f"}}>published at:</span><span style={{textShadow:"0 1px 1px black"}}>{news[numindex].published}</span></div><div><span style={{padding:"1rem",textShadow:"0 1px 1px #f0f"}}>Author:</span><span style={{textShadow:"0 1px 1px black"}}></span></div></div>
+       <div className={HomeStyle.infobox2}><p style={{padding:"1rem",textShadow:"0 1px 1px #f0f"}}>description:</p><p  style={{padding:"1rem",textShadow:"0 1px 1px black"}}>{news[numindex].title.substring(0, 160)}...</p><center><a href={news[numindex].link} className={homeStyle.seemorelink}>See more</a></center></div>
        </div>
         </div>
       </div>
